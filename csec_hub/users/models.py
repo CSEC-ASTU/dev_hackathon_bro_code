8a53c2e6e941ddba81482207c33214a2a203c4e1
Yonathan Assefa <yab95assefa@gmail.com>
2022-05-06 16:08:20 +0300

    user model created

---
+++ csec_hub/users/models.py
@@ -0,0 +1,60 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+
+class User(AbstractUser):
+    
+    SEXCHOICE = (
+        ('M', 'Male'),
+        ('F','Female'))
+    
+    telegram_username = models.CharField(max_length=255, blank=True, null=True)
+    sex = models.CharField(max_length=1, choices=SEXCHOICE)
+    profile_picture = models.ImageField(upload_to='')
+    phone = models.CharField(max_length=13)
+    
+    
+class Memebership(User):
+    user_id = models.CharField(max_length=255, blank=True, null=True)
+    member_of = models.ForeignKey('Division', on_delete=models.CASCADE, related_name='member_type')
+    authority = models.ForeignKey('Authority', on_delete=models.CASCADE, null=True, blank=True, related_name='authority')
+    is_accepted = models.BooleanField(default=False)
+    accepted_by = models.ForeignKey('Authority', on_delete=models.CASCADE, null=True, blank=True, related_name='accepted_by')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+
+    def __str__(self):
+        return self.username
+    
+    def add_to_group(self, member_of):
+        self.groups.add(member_of)
+        self.save()
+
+class Division(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    vacancy = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
+
+class Authority(models.Model):
+    position = models.CharField(max_length=255)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.position
+
+
+    
+
+
