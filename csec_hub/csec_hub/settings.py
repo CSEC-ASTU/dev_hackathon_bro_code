ddb7fd7c68a4db9a8f0394ab1e9aec766a0fa139
Natnael <56716832+Natty4@users.noreply.github.com>
2023-05-16 09:29:05 +0300

    Update Database configuration

--- csec_hub/csec_hub/settings.py
+++ csec_hub/csec_hub/settings.py
@@ -103,10 +103,24 @@ DATABASES = {
     }
 }
 
+
 # mysql database configuration
 # DATABASES = {
 #     'default': {
 #         'ENGINE': os.getenv("DB_ENGINE"),
+#         'NAME': os.getenv("DB_NAME"),
+#         'USER': os.getenv("DB_USER"),
+#         'PASSWORD': os.getenv("DB_PASSWORD"),
+#         'HOST': os.getenv("DB_HOST"),
+#         'PORT': os.getenv("DB_PORT"),
+#     }
+# }
+
+
+# PostgreSQL database configuration
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
 #         'NAME': os.getenv("DB_NAME"),
 #         'USER': os.getenv("DB_USER"),
 #         'PASSWORD': os.getenv("DB_PASSWORD"),
